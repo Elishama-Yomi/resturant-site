c74a6285dde94e06b6dd173a3496111118d3aeb2
pr-Git-hub <88314443+pr-Git-hub@users.noreply.github.com>
2022-04-27 15:52:11 +0530

    Update About.js

--- src/about/About.js
+++ src/about/About.js
@@ -1,7 +1,19 @@
 import './about.css'
 const About = () => { 
     return (
-        <h1>Home about</h1>
+        <div class="backg">
+            <div class="para">
+            <h1> About Us </h1>
+            <p> At the very heart of the cuisine, lie its distinct flavours and</p>
+            <p>preparation style.</p> 
+            <p>At Seafood Restaurant , we have always stayed true to</p>   
+            <p>this very essence of the cuisine.</p>  
+            <p>The dishes are prepared using only the freshest</p>  
+            <p>ingredients and unique combination of spices, which</p>  
+            <p>do justice to the rich heritage of the cuisine;</p>
+            <p>while maintaining superior levels of quality.</p>
+            </div>
+        </div>
     )
 }
 export default About;
